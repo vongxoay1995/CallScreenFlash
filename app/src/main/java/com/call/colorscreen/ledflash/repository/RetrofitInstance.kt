24aa74c15db4311a1008c11339fbb928f800f3c8
DESKTOP-IDAIFO1\TAN <tanpt@ecomobile.vn>
2023-04-25 00:40:00 +0700

    add code

--- app/src/main/java/com/call/colorscreen/ledflash/repository/RetrofitInstance.kt
+++ app/src/main/java/com/call/colorscreen/ledflash/repository/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private const val BASE_URL = "http://103.82.24.192"
+private const val BASE_URL = "http://tmobiles.xyz/"
 
 object RetrofitInstance {
 
